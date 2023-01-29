ea488225b0bbf6815c7100eee420908a4ff0bfec
Sam Francis <sam.p.f.94@gmail.com>
2023-01-29 15:07:47 +0000

    updated packages

--- packages.py
+++ packages.py
@@ -1,6 +1,6 @@
-###  testing git for the second time
+###  testing git for the third time
 
 # import packages
 import pandas as pd
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
