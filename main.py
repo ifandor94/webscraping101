91cb9c873a838ed8414d47dd658eb5d7ade4aab7
Sam Francis <sam.p.f.94@gmail.com>
2023-02-16 18:53:59 +0000

    added run log

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-###  testing git for the third time
+###  
 
 # import packages
 import pandas as pd
 import requests
+import sys
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # declare variables
 wiki_url = "https://en.wikipedia.org/wiki/List_of_professional_sports_leagues_by_revenue"
@@ -15,12 +17,13 @@ def extract_data():
     #check request is successful, if not exit
     print("Connected" if response_code == 200 else "Error: " + page.status_code)
     if response_code != 200:
-        return
+        status = "Error: connection failed"
+        return status
     
     soup = BeautifulSoup(page.content, 'html.parser')
     table = soup.find('table',{'class':'wikitable sortable'})
     # fix headers
-    list_headers = ["League", "Sport", "Country(ies)", "Season", "Level on Pyramid", "Teams", "Matches // Games // Events", "Revenue (M Euro)", "Revenue p/Team", "Revenue p/Match (thousands)", "Ref."]
+    list_headers = ["League", "Sport", "Country(ies)", "Season", "Level on Pyramid", "Teams", "Matches / Games / Events", "Revenue (M Euro)", "Revenue p/Team", "Revenue p/Match (thousands)", "Ref."]
     rows = []
     # find rows associated with the table and pass to a list
     data_rows = table.find_all('tr')
@@ -32,10 +35,11 @@ def extract_data():
         rows.append(beautified_value)
     # pass output to dataframe
     df = pd.DataFrame(columns=list_headers, data=rows)
+    status = 'Success'
 
-    return df
+    return df, status, rows
 
-def transform_data(df):
+def transform_data(df, rows):
     # cleanse data as needed
     #drop the reference column
     df.drop(columns='Ref.',inplace=True)
@@ -53,6 +57,32 @@ def transform_data(df):
         , 'Revenue p/Team':'float'
         , 'Revenue p/Match (thousands)':'float'}
         )
-    
+
+def log(message):
+    timestamp_format = '%Y-%h-%d-%H:%M-%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt", "a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+log("ETL Job Started")
+
+log("Extract phase started")
+extracted_data, status, rows = extract_data()
+log("Extract phase ended: " + status)
+
+if status != 'Success':
+    log("ETL Job Terminated: Unsuccessful")
+    sys.exit()
+
+log("Transform phase started")
+transform_data(extracted_data, rows)
+log("Transform phase complete")
+
+
+
+
+
+# next step is to create a run log and to save the data. Maybe CSV to start, then SQL?
 
 
